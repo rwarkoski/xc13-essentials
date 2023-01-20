711f99d4b65bf2021546640b323beaaf145b4517
Robert Warkoski <rwarkoski@icloud.com>
2023-01-19 23:50:05 -0500

    Initial Commit

---
+++ SourceControl2/SourceControl2App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControl2App.swift
+//  SourceControl2
+//
+//  Created by Robert Warkoski on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SourceControl2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
