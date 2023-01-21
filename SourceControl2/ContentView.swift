0b446542b985359ff342af484aa722731a7877a9
Robert Warkoski <rwarkoski@icloud.com>
2023-01-21 13:29:11 -0500

    Fixed bug a

--- SourceControl2/ContentView.swift
+++ SourceControl2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world updated!")
+            Text("Hello, world updated bug a!")
         }
         .padding()
     }
